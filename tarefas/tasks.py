e723c9187e5c149e9ab231c8ed3fe02a839a454a
neto <garibaldiviolin@gmail.com>
2021-05-22 19:59:02 -0300

    Add return to tasks

--- tarefas/tasks.py
+++ tarefas/tasks.py
@@ -4,18 +4,22 @@ from tarefa_example.celery_app import app
 @app.task(bind=True)
 def teste_echo(self):
     print('Essa ecoou!\n' * 3)
+    return "this"
 
 
 @app.task(bind=True)
 def tarefa_legal(self):
     print('Essa tarefa é bem legal!\n' * 3)
+    return "that"
 
 
 @app.task(bind=True)
 def olha_isso(self):
     print('Olha isso aqui!\n' * 3)
+    return "only"
 
 
 @app.task(bind=True)
 def agora_vai(self):
     print('Agora vai!!!!!\n' * 3)
+    return "go"
